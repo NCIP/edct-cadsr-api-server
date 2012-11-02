784883929d074810f1d73d195d0b5b5b65efc787
kaganli <kaganli>
2012-11-02 00:38:45 +0000

    Initial version of caDSR-API-Server project - required for form cart import for FormBuilder
    SVN-Revision: 45

---
+++ src/com/healthcit/cadsr/utils/CADSRUtils.java
@@ -0,0 +1,91 @@
+/*******************************************************************************
+ * Copyright (c) 2012 HealthCare It, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the BSD 3-Clause license
+ * which accompanies this distribution, and is available at
+ * http://directory.fsf.org/wiki/License:BSD_3Clause
+ * 
+ * Contributors:
+ *     HealthCare It, Inc - initial API and implementation
+ ******************************************************************************/
+package com.healthcit.cadsr.utils;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Collection;
+
+import javax.servlet.http.HttpServletRequest;
+
+import com.healthcit.cadsr.transformer.DataTransformer;
+import com.thoughtworks.xstream.XStream;
+
+public class CADSRUtils {
+	private static XStream xmlConverter = new XStream();
+	
+	public static String toXML( Object object ) {
+		return object == null ? null : xmlConverter.toXML( object );
+	}
+	
+	public static Object fromXML( String xml ) {
+		return xml == null ? null : xmlConverter.fromXML( xml );
+	}
+	
+
+	private static String read (InputStream is) throws IOException
+	{
+		String data = null;
+
+		if (is != null)
+		{
+			// read into a buffer
+			BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+			StringBuilder sb = new StringBuilder();
+			String line = null;
+			while ((line = reader.readLine()) != null)
+			{
+				sb.append(line + "\n");
+			}
+			data = sb.toString();
+		}
+
+		return data;
+	}
+
+	public static String read(HttpServletRequest request) 
+	{
+		String content = null;
+		
+		try {
+			InputStream ins = CADSRUtils.getInputStream( request );
+			if ( ins != null ) content = read( ins );
+		}catch( Exception e){ 
+			DataTransformer.log.error( e.getMessage() );
+		}
+		
+		return content;
+	}
+
+	@SuppressWarnings({"unchecked","rawtypes"})
+	public
+	static void clearList( Collection list ) {
+		if ( list != null ) {
+			list.removeAll(list);
+		}
+			
+	}
+
+	public static InputStream getInputStream( HttpServletRequest request ) {
+		InputStream ins = null;
+		try { 
+			ins = request.getInputStream();
+		} catch( IOException ioe ){ DataTransformer.log.error( "Could not get inputstream from request" );}
+		return ins;
+	}
+
+	public static String getXML( Object object ) {
+		return toXML( object );
+	}
+}
